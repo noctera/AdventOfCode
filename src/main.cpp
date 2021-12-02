2daa2a6a52229bfcef54221012965b0854bc253d
noctera <julian.jaksch@gmx.de>
2021-12-02 17:07:53 +0100

    fix: updated to relative paths

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,12 @@
 int main () {
 
     std::cout << "Sonar Sweep" << std::endl;
-    auto sonarSweepData = SonarSweep::loadFile ("/home/julian/AdventOfCode/Numbers/01.txt");
+    auto sonarSweepData = SonarSweep::loadFile ("../Numbers/01.txt");
     std::cout << "Part 1: " << SonarSweep::partOne (sonarSweepData) << std::endl;
     std::cout << "Part 2: " << SonarSweep::partTwo (sonarSweepData) << std::endl;
     std::cout << "--------------------------------------------" << std::endl;
     std::cout << "Dive!" << std::endl;
-    auto diveData = Dive::loadFile ("/home/julian/AdventOfCode/Numbers/02.txt");
+    auto diveData = Dive::loadFile ("../Numbers/02.txt");
     std::cout << "Part 1: " << Dive::partOne (diveData) << std::endl;
     std::cout << "Part 2: " << Dive::partTwo (diveData) << std::endl;
 }
